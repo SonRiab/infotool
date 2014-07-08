04be4995c055f4a67c2f1b2525d8581788477aa2
Rene Jablonski <rene@vnull.de>
2014-07-08 02:19:22 +0200

    [Fix] missing url patterns [Fix] some visual glitches

--- infotool/urls.py
+++ infotool/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from portal import views as portal_views
-from booking import views as booking_views
+#from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -28,7 +29,7 @@ urlpatterns = patterns('',
     url(r'^$', portal_views.IndexView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
-)
+)  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 site_pattern = patterns('',
     url(r'(?P<pk>\d+)/', portal_views.SiteView.as_view(), ),
@@ -37,4 +38,7 @@ site_pattern = patterns('',
 urlpatterns += i18n_patterns('',
     url(r'^$', portal_views.IndexView.as_view(), name='index'),
     url(r'site/', include(site_pattern, namespace='site'), ),
+    url(r'feedback/$', portal_views.FeedbackView.as_view()),
+    url(r'damage-report/$', portal_views.DamageReportView.as_view()),
+    url(r'(feedback|damage-report)/success/$', portal_views.FormSuccessView.as_view()),
 )
